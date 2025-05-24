21db6cee34996ef23fe883370617e6205b295b32
stroberinanas <sabilakhairina2@gmail.com>
2025-05-24 20:10:16 +0700

    Task 7

---
+++ model/UserModel.js
@@ -0,0 +1,36 @@
+// import { Sequelize } from "sequelize";
+// import db from "../config/database.js";
+
+// const Notes = db.define("notes", {
+//     title: Sequelize.STRING,
+//     category: Sequelize.STRING,
+//     note: Sequelize.STRING,
+// });
+
+// const User = db.define("user", {
+//     name: Sequelize.STRING,
+//     email: Sequelize.STRING,
+//     password: Sequelize.STRING,
+//     refresh_token: Sequelize.TEXT,
+// });
+
+// db.sync().then(() => console.log("database synced"));
+// export default { Notes, User };
+
+import { Sequelize } from "sequelize";
+import db from "../config/database.js";
+
+export const Notes = db.define("notes", {
+    title: Sequelize.STRING,
+    category: Sequelize.STRING,
+    note: Sequelize.STRING,
+});
+
+export const User = db.define("user", {
+    name: Sequelize.STRING,
+    email: Sequelize.STRING,
+    password: Sequelize.STRING,
+    refresh_token: Sequelize.TEXT,
+});
+
+db.sync().then(() => console.log("database synced"));
